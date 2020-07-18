77b3ffeb537bda879c496e100a0f6a5d85e0aec2
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-07-17 23:08:34 -0500

    ADD - chat component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
+
+import MainLayout from "./components/layouts/MainLayout";
+import Chat from "./components/chat/Chat.jsx";
+
 import "./App.css";
 
-const App = () => <div className="App"></div>;
+const App = () => (
+  <div className="App">
+    <MainLayout>
+      <Chat />
+    </MainLayout>
+  </div>
+);
 
 export default App;
---
+++ client/src/components/chat/Chat.jsx
@@ -0,0 +1,46 @@
+import React, { useState, createRef } from "react";
+
+import InputText from "./InputText";
+import MessageList from "./MessageList";
+
+const initMessage = {
+  user: "bot",
+  message: "¡Hola!, soy tu asistente virtual. Espero poder ayudarte.",
+};
+
+const Chat = () => {
+  const [messages, setMessages] = useState([initMessage]);
+  const messagesRef = createRef();
+
+  const sendMessage = (message) => {
+    setMessages([...messages, { message, user: "human" }]);
+    //TODO
+  };
+
+  const scrollToBottom = () => {
+    if (messagesRef.current) {
+      messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+    }
+  };
+
+  return (
+    <div>
+      <div className="chat_window">
+        <div className="top_menu">
+          <div className="buttons">
+            <div className="button close" />
+            <div className="button minimize" />
+            <div className="button maximize" />
+          </div>
+          <div className="title">Dialogflow Chatbot</div>
+        </div>
+        <ul className="messages" ref={messagesRef}>
+          <MessageList messageList={messages} />
+        </ul>
+        <InputText sendMessage={sendMessage} />
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
